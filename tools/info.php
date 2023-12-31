f0e91c78ead6db8306e7c3baf0ed4c24fe2e9ea6
Jz-powpow <113581514+Jz-powpow@users.noreply.github.com>
2023-12-31 04:39:38 -0300

    info.php

--- tools/info.php
+++ tools/info.php
@@ -25,7 +25,7 @@ if (preg_match('/^(\/id|\.id|!info)/', $text)) {
 
     $photoId = getUserProfilePhoto($userId);
 
-    $m = "<b>[火] User information %0A╔═════════════════╗%0A•├User[NAME] » @$username%0A•├User[ID] » <code>$userId</code>%0A•├Name[TG] » $firstname%0A•├User[RANK] » <code>$rank</code>%0A╚═════════════════╝%0A•├Dev » <code>@EscaliBud</code></b>";
+    $m = "<b>[火] User info %0A╔═════════════════╗%0A•├User[NAME] » @$username%0A•├User[ID] » <code>$userId</code>%0A•├Name[TG] » $firstname%0A•├User[RANK] » <code>$rank</code>%0A╚═════════════════╝%0A•𝘿𝙚𝙫 𝙗𝙮 » <code>@hexnynejz</code></b>";
 
     if ($photoId) {
         sendPhotox($chatId, $photoId, $m);
