493c777f7730902588d3ab82a3de3977e3e808da
Jz-powpow <113581514+Jz-powpow@users.noreply.github.com>
2023-12-31 23:12:39 -0300

    mchk.php

--- gates/mchk.php
+++ gates/mchk.php
@@ -59,7 +59,7 @@ $text = $update["message"]["text"];
 //========WHO CAN CHECK FUNC========//
 
 //=====WHO CAN CHECK FUNC END======//
-if (preg_match('/^(\/ax|\.mchk|!ax)/', $text)) {
+if (preg_match('/^(\/ccn|\.ccn|!ccn)/', $text)) {
     $userid = $update['message']['from']['id'];
 
     if (!checkAccess($userid)) {
